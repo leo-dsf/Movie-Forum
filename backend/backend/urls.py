923576dec7722d4baadaecca7eea5c3b8a944db7
Leonardo Flórido <leonardo.s.florido@outlook.com>
2022-12-26 15:44:12 +0000

    Need to implement some web services for the user and login with token.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from app import views
 
@@ -27,6 +28,7 @@ schema_view = get_schema_view(
         description="REST API for Movie Forum",
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
